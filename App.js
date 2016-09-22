f5e96457122dea7712198ff354a2b0b48bee181b
Augustin <Augustinraj@users.noreply.github.com>
2016-09-22 12:17:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+var details = {
+
+    "name": "Augus",
+    "Age": "26",
+    "Date of birth": "17.05.1992",
+    "place of Birth": "chennai"
+
+};
+// to Display our name, Age, Date of Birth, place of Birth//
+console.log(details);
